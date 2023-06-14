dd0cd575479a65598aebacea774d6a6b861a59e1
GrimmOT <ashutov2009@gmail.com>
2023-06-14 19:20:33 +0300

    Todo

--- TodoList/TodoList/urls.py
+++ TodoList/TodoList/urls.py
@@ -24,5 +24,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    
 ]
 
--- TodoList/Todoapp/views.py
+++ TodoList/Todoapp/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from Todoapp.models import *
 
 # Create your views here.
 
 def index(request):
+    tasks = Task.objects.all()
     context = {
-
+        "title": "TODO list",
+        "tasks": tasks
     }
-    return render(request, "index.html") 
+    return render(request, "index.html", context) 
