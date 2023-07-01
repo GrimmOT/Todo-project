cb08b125a60e3a4ce3787cf960f52068e79acd30
GrimmOT <ashutov2009@gmail.com>
2023-07-01 16:40:45 +0300

    Todo

--- TodoList/Todoapp/views.py
+++ TodoList/Todoapp/views.py
@@ -4,6 +4,15 @@ from Todoapp.models import *
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        print("Пришли данные")
+        if "task" in request.POST:
+            print(request.POST["task"])
+            # Создание новой записи
+            new_task = Task(text=request.POST["task"])
+            # Сохранение новой записи
+            new_task.save()
+
     tasks = Task.objects.all()
     context = {
         "title": "TODO list",
